d1587c38e7b3fd629c356fda3ea9605266a01f09
Oli-Dom <oadl12345678@gmail.com>
2023-08-02 18:55:31 -0400

    Generated base structure of chatbot

---
+++ financebot/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import FinancialAdvisorApp from './FinancialAdvisorApp';
+
+function App() {
+  return (
+    <div className="App">
+      <FinancialAdvisorApp />
+    </div>
+  );
+}
+
+export default App;
+
+
