0a327a0dcdd8991e6cc2added6169becdd413f25
Oli-Dom <oadl12345678@gmail.com>
2023-08-03 14:19:21 -0400

    Env variable change, and setting up arrays

--- finaceBot/financial-advisor-app/src/FinancialAdvisorApp.js
+++ finaceBot/financial-advisor-app/src/FinancialAdvisorApp.js
@@ -1,7 +1,25 @@
 // Import necessary modules and components
-import React, { useState } from 'react';
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+import React, { useState } from "react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+
+const questions = [
+  "How can I build my credit score?",
+  "How can I start a businesss?",
+  "How can I plan for college?",
+];
+const answers = [
+  "To build your credit score, check your credit report for errors, use a secured credit card or credit builder loan, and always make timely payments. Keep your credit utilization below 30%, avoid opening too many new accounts, and maintain old accounts for a longer credit history. Apply for new credit only when needed, monitor your credit regularly, and be patient while practicing responsible financial habits. Building credit takes time, but these steps will help you establish a strong credit profile.",
+  "To start a business, you'll need to follow these key steps. First, conduct thorough market research to identify your target audience and assess the demand for your product or service. Next, create a detailed business plan outlining your objectives, strategies, and financial projections. Register your business name and structure, obtain necessary permits and licenses, and secure funding through personal savings, loans, or investors. Build a strong team and network with industry professionals. Establish your online and offline presence through a website, social media, and marketing efforts. Lastly, launch your business and continually adapt and grow based on customer feedback and market trends. Remember, entrepreneurship requires dedication, resilience, and adaptability.",
+  "To plan for college successfully, consider the following steps. Start by researching potential colleges and universities that offer programs aligning with your interests and career goals. Review admission requirements and deadlines, and gather information on financial aid options such as scholarships, grants, and loans. Create a timeline to track application deadlines, standardized tests (SAT/ACT), and other essential documents needed for admission. Work on enhancing your academic performance and participating in extracurricular activities to strengthen your college applications. Seek guidance from school counselors or mentors to navigate the process. Lastly, be proactive in exploring different career paths and college majors to make an informed decision that aligns with your aspirations. Planning ahead will increase your chances of a successful college experience.",
+];
 
 // Define the main component
 const FinancialAdvisorApp = () => {
@@ -10,21 +28,20 @@ const FinancialAdvisorApp = () => {
     {
       message: "Hello, I'm Kira, your financial advisor",
       sentTime: "just now",
-      sender: "Kira"
-    }
+      sender: "Kira",
+    },
   ]);
   const [isTyping, setIsTyping] = useState(false);
 
-  // Replace this API key with your own OpenAI API key
-  const API_KEY = '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';
+  const apiKey = process.env.REACT_APP_API_KEY;
 
   // Function to handle user message sending
   const handleSend = async (message) => {
     // Create a new message object with user's message
     const newMessage = {
       message,
-      direction: 'outgoing',
-      sender: "user"
+      direction: "outgoing",
+      sender: "user",
     };
 
     // Add the new message to the messages state
@@ -48,29 +65,32 @@ const FinancialAdvisorApp = () => {
       } else {
         role = "user";
       }
-      return { role: role, content: messageObject.message }
+      return { role: role, content: messageObject.message };
     });
 
     // Prepare the API request body
     const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        { role: 'system', content: 'You are a helpful financial assistant.' }, // The system message defines the logic of our chatGPT
-        ...apiMessages
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "You are a helpful financial assistant." }, // The system message defines the logic of our chatGPT
+        ...apiMessages,
       ],
-      "max_tokens": 100 // Limit the response to 100 tokens
-    }
+      max_tokens: 200, // Limit the response
+    };
 
     try {
       // Make the API call to OpenAI to get the response
-      const response = await fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
-        headers: {
-          "Authorization": "Bearer " + API_KEY,
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(apiRequestBody)
-      });
+      const response = await fetch(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          method: "POST",
+          headers: {
+            Authorization: "Bearer " + apiKey,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(apiRequestBody),
+        }
+      );
 
       // Parse the API response data
       const data = await response.json();
@@ -79,15 +99,18 @@ const FinancialAdvisorApp = () => {
       // Check if the response contains choices and at least one message
       if (data.choices && data.choices.length > 0) {
         // Add the response from Kira as a new message to the chat
-        setMessages([...chatMessages, {
-          message: data.choices[0].message['content'], // Get the content of the response
-          sender: "Kira"
-        }]);
+        setMessages([
+          ...chatMessages,
+          {
+            message: data.choices[0].message["content"], // Get the content of the response
+            sender: "Kira",
+          },
+        ]);
       } else {
         console.error("No response content found.");
       }
     } catch (error) {
-      console.error('Error fetching the answer:', error);
+      console.error("Error fetching the answer:", error);
     }
 
     // Set the "Kira is typing" indicator to false after processing the response
@@ -96,15 +119,27 @@ const FinancialAdvisorApp = () => {
 
   return (
     // Render the chat UI components
-    <div className="App" style={{ height: "100vh" }}>
-      <MainContainer>
+    <div
+      className="App"
+      style={{
+        height: "80vh",
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "2.5rem",
+      }}
+    >
+      <MainContainer
+        style={{ maxWidth: "600px", width: "90%", height: "80vh" }}
+      >
         <ChatContainer>
           <MessageList
-            typingIndicator={isTyping ? <TypingIndicator content="Kira is typing" /> : null}
-            style={{ height: "calc(100vh - 100px)" }} // Adjust the height to cover the entire page (minus header)
+            typingIndicator={
+              isTyping ? <TypingIndicator content="Kira is typing" /> : null
+            }
+            style={{ maxHeight: "calc(100% - 40px)", overflowY: "auto" }} // Adjust the max-height and overflow for scrolling
           >
             {messages.map((message, i) => {
-              return <Message key={i} model={message} />
+              return <Message key={i} model={message} />;
             })}
           </MessageList>
           <MessageInput placeholder="Type message here" onSend={handleSend} />
