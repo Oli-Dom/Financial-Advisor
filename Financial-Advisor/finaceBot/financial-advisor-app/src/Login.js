b0c935bd5aa79b184c9a5128b58361ac207c8545
Factorial343 <rwilliamsofficial04@gmail.com>
2023-08-03 21:49:02 -0400

    Login page & part of home

--- Financial-Advisor/finaceBot/financial-advisor-app/src/Login.js
+++ Financial-Advisor/finaceBot/financial-advisor-app/src/Login.js
@@ -4,15 +4,34 @@ import "./Login.css"
 
 const Login = () => {
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <div className="input-container">
-        <input type="text" placeholder="Username" className="input-field" />
-        <input type="password" placeholder="Password" className="input-field" />
-        {/* Link to Home component */}
-        <Link to="/home">
-          <button className="login-btn">Login</button>
-        </Link>
+    <div className="login_body">
+      <div className="container">
+        <div className="screen">
+          <div className="screen__content">
+            <form className="login">
+              <div className="login__field">
+                <i className="login__icon fas fa-user" />
+                <input type="text" className="login__input" placeholder="User name / Email" />
+              </div>
+              <div className="login__field">
+                <i className="login__icon fas fa-lock" />
+                <input type="password" className="login__input" placeholder="Password" />
+              </div>
+              <Link to="/home">
+                <button className="button login__submit">
+                  <span className="button__text">Log In Now</span>
+                  <i className="button__icon fas fa-chevron-right" />
+                </button>
+              </Link>
+            </form>
+          </div>
+          <div className="screen__background">
+            <span className="screen__background__shape screen__background__shape4" />
+            <span className="screen__background__shape screen__background__shape3" />
+            <span className="screen__background__shape screen__background__shape2" />
+            <span className="screen__background__shape screen__background__shape1" />
+          </div>
+        </div>
       </div>
     </div>
   );
