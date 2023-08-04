cebd23f257515f38b5293ab4f6b8e64c1046684b
Overload02 <alvajoy123@gmail.com>
2023-08-04 01:58:29 -0400

    Added styling to the home page!

--- Financial-Advisor/finaceBot/financial-advisor-app/src/Home.js
+++ Financial-Advisor/finaceBot/financial-advisor-app/src/Home.js
@@ -14,36 +14,38 @@ function HomePage() {
         <table>
           {/* Feature cards */}
           <tr>
-            <div className="feature">
-              <div className="feature-icon"></div>
-              <h3>What is Kira?</h3>
-              <p>
-                Kira is an innovative and inclusive financial AI assistant,
-                <br></br>
-                specifically designed to cater to the needs of minorities. With
-                a<br></br>
-                strong focus on personalization and cultural sensitivity, Kira
-                <br></br>
-                serves as a powerful tool providing essential financial guidance
-                and
-                <br></br>
-                support to individuals from diverse backgrounds. As a virtual
-                <br></br>
-                assistant, Kira leverages advanced artificial intelligence
-                <br></br>
-                algorithms and data analytics to gain a deep understanding of
-                the
-                <br></br>
-                unique financial challenges faced by minorities. This allows
-                Kira to
-                <br></br>
-                offer tailored and relevant solutions to enhance their overall
-                <br></br>
-                financial well-being.
-              </p>
-            </div>
-          </tr>
-          <tr>
+
+            <th>
+              <div className="feature">
+                <div className="feature-icon"></div>
+                <h3>What is Kira?</h3>
+                <p>
+                  Kira is an innovative and inclusive financial AI assistant,
+                  <br></br>
+                  specifically designed to cater to the needs of minorities. With
+                  a<br></br>
+                  strong focus on personalization and cultural sensitivity, Kira
+                  <br></br>
+                  serves as a powerful tool providing essential financial guidance
+                  and
+                  <br></br>
+                  support to individuals from diverse backgrounds. As a virtual
+                  <br></br>
+                  assistant, Kira leverages advanced artificial intelligence
+                  <br></br>
+                  algorithms and data analytics to gain a deep understanding of
+                  the
+                  <br></br>
+                  unique financial challenges faced by minorities. This allows
+                  Kira to
+                  <br></br>
+                  offer tailored and relevant solutions to enhance their overall
+                  <br></br>
+                  financial well-being.
+                </p>
+              </div>
+            </th>
+
             <th>
               <div>
                 <img
@@ -53,33 +55,46 @@ function HomePage() {
               </div>
             </th>
           </tr>
+
+          <tr>
+            <td>
+              <div>
+                <img src="https://img.freepik.com/premium-vector/finance-concept-with-character-situation-man-brainstorming-generates-new-ideas-development-financial-wealth-making-money-vector-illustration-with-people-scene-flat-design-web_9209-11304.jpg?w=2000"></img>
+              </div>
+            </td>
+
+            <td>
+              <div className="feature">
+                <div className="feature-icon"></div>
+                <h3>Why Kira?</h3>
+                <p>
+                  Kira is specifically designed to understand the unique financial
+                  <br></br>
+                  challenges and needs of minorities. It provides personalized and
+                  <br></br>
+                  culturally sensitive financial advice that addresses your specific
+                  <br></br>
+                  circumstances, helping you make informed decisions. Kira is<br></br>
+                  committed to being accessible and inclusive to all users. It ensures
+                  <br></br>
+                  that language, cultural nuances, and diverse financial situations
+                  <br></br>
+                  are considered, creating a safe and welcoming environment for
+                  <br></br>
+                  minorities to engage with their finances.
+                </p>
+              </div>
+
+            </td>
+
+          </tr>
+
+
         </table>
 
-      <br></br>
-        <div>
-          <img src="https://img.freepik.com/premium-vector/finance-concept-with-character-situation-man-brainstorming-generates-new-ideas-development-financial-wealth-making-money-vector-illustration-with-people-scene-flat-design-web_9209-11304.jpg?w=2000"></img>
-        </div>
+        <br></br>
 
-        <div className="feature">
-          <div className="feature-icon"></div>
-          <h3>Why Kira?</h3>
-          <p>
-            Kira is specifically designed to understand the unique financial
-            <br></br>
-            challenges and needs of minorities. It provides personalized and
-            <br></br>
-            culturally sensitive financial advice that addresses your specific
-            <br></br>
-            circumstances, helping you make informed decisions. Kira is<br></br>
-            committed to being accessible and inclusive to all users. It ensures
-            <br></br>
-            that language, cultural nuances, and diverse financial situations
-            <br></br>
-            are considered, creating a safe and welcoming environment for
-            <br></br>
-            minorities to engage with their finances.
-          </p>
-        </div>
+
 
         {/* Repeat for other features */}
       </section>
