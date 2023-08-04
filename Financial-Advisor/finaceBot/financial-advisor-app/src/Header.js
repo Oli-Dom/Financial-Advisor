dcb2d27561a3b6cf62c28af0d39b81d285ea3f72
Overload02 <alvajoy123@gmail.com>
2023-08-04 01:16:30 -0400

    updated the header!

--- Financial-Advisor/finaceBot/financial-advisor-app/src/Header.js
+++ Financial-Advisor/finaceBot/financial-advisor-app/src/Header.js
@@ -1,15 +1,19 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import logo from './images/logo.png';
 import './Header.css';
 
 const Header = () => {
     return (
         <div className="header">
-            <h1 className="logo"> Kira</h1>
+            <img src = {logo} alt = "logo" className="logo_icon" />
+
             <div className="links">
-                <Link to="/home">Home</Link>
-               
-                <Link to="/advisor">Try Me</Link>
+                <Link to="/home">HOME</Link>
+                <Link to="/home">ABOUT</Link>
+                <Link to="/home">CONTACT US</Link>
+                <Link to="/advisor"><p className="logo">TRY ME!</p></Link>
             </div>
+            
         </div>
     );
 }
